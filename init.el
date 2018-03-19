0c1d1850b56ec182b42b84c9135243ec6779b003
Nlsnightmare < >
2018-03-19 10:44:57 +0200

    Added comments, deleted vertival ido

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
     ("a94f1a015878c5f00afab321e4fef124b2fc3b823c8ddd89d360d710fc2bddfc" "6dd2b995238b4943431af56c5c9c0c825258c2de87b6c936ee88d6bb1e577cb9" default)))
  '(package-selected-packages
    (quote
-    (helm try flycheck company evil-visual-replace evil-numbers yasnippet web-mode neotree emmet-mode ido-vertical-mode hl-todo rainbow-delimiters company-c-headers company-tern elpy irony-eldoc iront-eldoc company-irony irony-mode flycheck-irony flycheck-mode js2-mode tern tern-mode dashboard key-chord all-the-icons company-lua lua-mode airline-themes powerline atom-one-dark-theme atom-one-dark evil-surround evil-leader evil use-package org-bullets))))
+    (helm try flycheck company evil-visual-replace evil-numbers yasnippet web-mode neotree emmet-mode hl-todo rainbow-delimiters company-c-headers company-tern elpy irony-eldoc iront-eldoc company-irony irony-mode flycheck-irony flycheck-mode js2-mode tern tern-mode dashboard key-chord all-the-icons company-lua lua-mode airline-themes powerline atom-one-dark-theme atom-one-dark evil-surround evil-leader evil use-package org-bullets))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
